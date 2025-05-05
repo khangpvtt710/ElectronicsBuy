380236d1068b45dcdec82e61d275478ddf1c9388
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-05-05 17:25:28 +0700

    f
    f

---
+++ admin/slider.php
@@ -0,0 +1,23 @@
+<section class="admin-content">
+    <div class="admin-content-left">
+        <ul>
+            <li><a href="#">Danh Mục</a>
+                <ul>
+                    <li><a href="#">Thêm danh mục</a></li>
+                    <li><a href="#">Danh sách danh mục</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Loại Sản Phẩm</a>
+                <ul>
+                    <li><a href="#">Thêm loại Sản Phẩm</a></li>
+                    <li><a href="#">Danh sách loại Sản Phẩm</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Sản Phẩm</a>
+                <ul>
+                    <li><a href="#">Thêm Sản Phẩm</a></li>
+                    <li><a href="#">Danh sách Sản Phẩm</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
